9f457c801237eb6f789b6e6bd90589e5915bb833
donisdonatas <donisdonatas@gmail.com>
2023-01-05 21:15:09 +0200

    PIN Code

---
+++ Bankomatas/System/PIN.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankomatas.System
+{
+    public static class PIN
+    {
+        public static string InputPIN()
+        {
+            string PIN = "";
+            Console.ForegroundColor = ConsoleColor.White;
+            ConsoleKeyInfo Keyboard = Console.ReadKey(true);
+            while (Keyboard.Key != ConsoleKey.Enter & PIN.Length < 4)
+            {
+                if (Keyboard.Key != ConsoleKey.Backspace)
+                {
+                    Console.Write("*");
+                    PIN += Keyboard.KeyChar;
+                }
+                else if (Keyboard.Key == ConsoleKey.Backspace)
+                {
+                    if (!string.IsNullOrEmpty(PIN))
+                    {
+                        // remove one character from the list of password characters
+                        PIN = PIN.Substring(0, PIN.Length - 1);
+                        // get the location of the cursor
+                        int pos = Console.CursorLeft;
+                        // move the cursor to the left by one character
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                        // replace it with space
+                        Console.Write(" ");
+                        // move the cursor to the left by one character again
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                    }
+                }
+                Keyboard = Console.ReadKey(true);
+            }
+
+            // add a new line because user pressed enter at the end of their password
+            Console.WriteLine();
+            return PIN;
+        }
+    }
+}
