b7f5b9e1281a58f4fea3b2224203143421a3a971
donisdonatas <donisdonatas@gmail.com>
2023-01-06 22:32:48 +0200

    Installed System.Data.SQLite

---
+++ Bankomatas/Menu.cs
@@ -0,0 +1,61 @@
+﻿using Bankomatas.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bankomatas
+{
+    public static class Menu
+    {
+        public static void PrimaryMenu()
+        {
+            int LoginAttemps = 0;
+            bool isLoginOK = false;
+            string Pin = "";
+
+            //Reikia daryti atskirus Metodus
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Sveiki atvykę čia Bankomatas.");
+            Console.WriteLine("Norėdami atlikti operacijas turite patvirtinti autorizaciją.");
+            Console.WriteLine("Įdėkite kortelę:");
+            Console.ForegroundColor = ConsoleColor.White;
+            //Galima padaryti Card Class, kuri iškvies įvedimą inicijuojant ir paskui turės metodą patikrinti atitikmenį. Tas pats ir su PIN.
+            string Card = Console.ReadLine();
+            bool isCardOK = true;
+
+            while (LoginAttemps < 3 && !isLoginOK)
+            {
+                
+                
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Įveskite keturių skaitmenų PIN kodą:");
+                Pin = PIN.InputPIN();
+                bool isPinOK = false;
+
+                if(isCardOK && isPinOK)
+                {
+                    isLoginOK = true;
+                }
+                else
+                {
+                    LoginAttemps++;
+                    if (LoginAttemps == 3)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("3 kartus blogai įvestas slaptažodis.");
+                        Console.WriteLine("Kortelė užblokuota");
+                        Thread.Sleep(2000);
+                        Environment.Exit(0);
+                    }
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Blogai įvestas slaptažodis. Bandykite dar kartą.");
+                    Console.WriteLine("Liko bandymų {0}", 3 - LoginAttemps);
+                }
+            }
+            Console.WriteLine(Pin);
+        }
+    }
+}
--- Bankomatas/Program.cs
+++ Bankomatas/Program.cs
@@ -12,17 +12,9 @@ namespace Bankomatas
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Sveiki atvykę čia Bankomatas.");
-            Console.WriteLine("Norėdami atlikti operacijas turite patvirtinti autorizaciją.");
-            Console.WriteLine("Įdėkite kortelę:");
-            Console.ForegroundColor = ConsoleColor.White;
-            string Card = Console.ReadLine();
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Įveskite keturių skaitmenų PIN kodą:");
-            string Pin = PIN.InputPIN();
-            
-            Console.WriteLine(Pin);
+            Menu.PrimaryMenu();
+
+
             Console.ReadLine();
         }
     }
